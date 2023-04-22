b6d1fce3829255cc420d1701b11c7cb7176c5c92
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-04-22 21:18:40 +0200

    refactor: Outsource URLImporter.dart

--- lib/screens/ImportTaskScreen.dart
+++ lib/screens/ImportTaskScreen.dart
@@ -1,8 +1,7 @@
-import 'package:clipboard_watcher/clipboard_watcher.dart';
 import 'package:enough_platform_widgets/enough_platform_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:locus/constants/spacing.dart';
+import 'package:locus/screens/import_task_screen_widgets/URLImporter.dart';
 import 'package:locus/services/view_service.dart';
 
 import '../utils/theme.dart';
@@ -26,49 +25,11 @@ class ImportTaskScreen extends StatefulWidget {
   State<ImportTaskScreen> createState() => _ImportTaskScreenState();
 }
 
-class _ImportTaskScreenState extends State<ImportTaskScreen> with ClipboardListener {
+class _ImportTaskScreenState extends State<ImportTaskScreen> {
   final TextEditingController _urlController = TextEditingController();
   final PageController _pageController = PageController();
-  String? _clipboard;
   TaskImportProgress? _progress;
   TaskView? _taskView;
-
-  @override
-  void initState() {
-    super.initState();
-
-    clipboardWatcher.addListener(this);
-    clipboardWatcher.start();
-    onClipboardChanged();
-  }
-
-  @override
-  void dispose() {
-    _urlController.dispose();
-    _pageController.dispose();
-
-    clipboardWatcher.removeListener(this);
-    clipboardWatcher.stop();
-
-    super.dispose();
-  }
-
-  @override
-  void onClipboardChanged() async {
-    setState(() {
-      _clipboard = null;
-    });
-
-    final newClipboardData = await Clipboard.getData(Clipboard.kTextPlain);
-
-    final result = Uri.tryParse(newClipboardData?.text ?? "");
-
-    if (result?.hasAbsolutePath ?? false) {
-      setState(() {
-        _clipboard = result!.toString();
-      });
-    }
-  }
 
   Future<void> importFromLink() async {
     try {
@@ -133,55 +94,9 @@ class _ImportTaskScreenState extends State<ImportTaskScreen> with ClipboardListe
                     ),
                   ],
                 ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Flexible(
-                      child: PlatformTextField(
-                        controller: _urlController,
-                        enabled: _progress == null,
-                        textInputAction: TextInputAction.done,
-                        keyboardType: TextInputType.url,
-                        hintText: "https://locus.app#",
-                        material: (_, __) =>
-                            MaterialTextFieldData(
-                              decoration: InputDecoration(
-                                labelText: "URL",
-                                border: _clipboard == null
-                                    ? OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(MEDIUM_SPACE),
-                                )
-                                    : OutlineInputBorder(
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(MEDIUM_SPACE),
-                                    bottomLeft: Radius.circular(MEDIUM_SPACE),
-                                  ),
-                                ),
-                              ),
-                            ),
-                      ),
-                    ),
-                    if (_clipboard != null)
-                      PlatformElevatedButton(
-                        padding: const EdgeInsets.all(MEDIUM_SPACE),
-                        onPressed: () {
-                          _urlController.text = _clipboard!;
-                        },
-                        child: Icon(Icons.paste_rounded),
-                        material: (_, __) =>
-                            MaterialElevatedButtonData(
-                              style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.all(MEDIUM_SPACE - 1),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                    topRight: Radius.circular(MEDIUM_SPACE),
-                                    bottomRight: Radius.circular(MEDIUM_SPACE),
-                                  ),
-                                ),
-                              ),
-                            ),
-                      ),
-                  ],
+                URLImporter(
+                  controller: _urlController,
+                  enabled: _progress == null,
                 ),
                 if (_progress != null) ...[
                   Center(
