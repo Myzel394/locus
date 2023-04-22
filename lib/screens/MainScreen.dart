79675fd4f368784a9222ba62192d6f3e97bcd407
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-04-22 22:44:37 +0200

    ui: Add padding

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -8,6 +8,7 @@ import 'package:locus/services/task_service.dart';
 import 'package:locus/widgets/Paper.dart';
 import 'package:provider/provider.dart';
 
+import '../utils/theme.dart';
 import 'CreateTaskScreen.dart';
 import 'main_screen_widgets/CreateTask.dart';
 
@@ -29,6 +30,12 @@ class _MainScreenState extends State<MainScreen> {
   bool listViewShouldFillUp = false;
   double listViewHeight = 0;
 
+  double get windowHeight =>
+      MediaQuery
+          .of(context)
+          .size
+          .height - kToolbarHeight;
+
   // If the ListView covers more than 75% of the screen, then actions get a whole screen of space.
   // Otherwise fill up the remaining space.
   bool getShouldUseScreenHeight(final BuildContext context) {
@@ -38,17 +45,15 @@ class _MainScreenState extends State<MainScreen> {
     }
 
     final listViewHeight = listViewKey.currentContext?.size?.height ?? 0;
-    final screenHeight = MediaQuery.of(context).size.height;
-    return listViewHeight > screenHeight * 0.75;
+    return listViewHeight > windowHeight * 0.6;
   }
 
   // Checks if the ListView should fill up the remaining space. This means that the listView is smaller than the
   // remaining height.
   bool getListViewShouldFillUp(final BuildContext context) {
     final listViewHeight = listViewKey.currentContext?.size?.height ?? 0;
-    final screenHeight = MediaQuery.of(context).size.height;
 
-    return listViewHeight < screenHeight;
+    return listViewHeight < windowHeight;
   }
 
   @override
@@ -84,97 +89,123 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     final taskService = context.watch<TaskService>();
-    final windowHeight = MediaQuery.of(context).size.height;
 
     return PlatformScaffold(
-      material: (_, __) => MaterialScaffoldData(
-        floatingActionButton: taskService.tasks.isEmpty
-            ? null
-            : OpenContainer(
-                transitionDuration: const Duration(milliseconds: 500),
-                transitionType: ContainerTransitionType.fade,
-                openBuilder: (context, action) => const CreateTaskScreen(),
-                closedBuilder: (context, action) => SizedBox(
-                  height: FAB_DIMENSION,
-                  width: FAB_DIMENSION,
-                  child: Center(
-                    child: Icon(
-                      context.platformIcons.add,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  ),
-                ),
-                closedElevation: 6.0,
-                closedShape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(FAB_DIMENSION / 2),
-                  ),
-                ),
-                openColor: Theme.of(context).scaffoldBackgroundColor,
-                closedColor: Theme.of(context).colorScheme.primary,
-              ),
-      ),
-      body: SingleChildScrollView(
-        child: taskService.tasks.isEmpty
-            ? Column(
-                children: <Widget>[
+      material: (_, __) =>
+          MaterialScaffoldData(
+            floatingActionButton: taskService.tasks.isEmpty
+                ? null
+                : OpenContainer(
+              transitionDuration: const Duration(milliseconds: 500),
+              transitionType: ContainerTransitionType.fade,
+              openBuilder: (context, action) => const CreateTaskScreen(),
+              closedBuilder: (context, action) =>
                   SizedBox(
-                    height: windowHeight,
+                    height: FAB_DIMENSION,
+                    width: FAB_DIMENSION,
                     child: Center(
-                      child: CreateTask(),
-                    ),
-                  ),
-                  SizedBox(
-                    height: windowHeight,
-                    child: Center(
-                      child: ImportTask(),
-                    ),
-                  ),
-                ],
-              )
-            : Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    SizedBox(
-                      height: (() {
-                        if (shouldUseScreenHeight) {
-                          if (listViewShouldFillUp) {
-                            return windowHeight;
-                          }
-                        }
-
-                        return null;
-                      })(),
-                      child: ListView.builder(
-                        key: listViewKey,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: taskService.tasks.length,
-                        itemBuilder: (context, index) {
-                          final task = taskService.tasks[index];
-
-                          return TaskTile(
-                            task: task,
-                          );
-                        },
+                      child: Icon(
+                        context.platformIcons.add,
+                        color: Theme
+                            .of(context)
+                            .colorScheme
+                            .onPrimary,
                       ),
                     ),
-                    Container(
-                      height: shouldUseScreenHeight ? windowHeight : windowHeight - listViewHeight,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: MEDIUM_SPACE, vertical: LARGE_SPACE),
-                        child: Center(
-                          child: Paper(
-                            child: ImportTask(),
+                  ),
+              closedElevation: 6.0,
+              closedShape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(FAB_DIMENSION / 2),
+                ),
+              ),
+              openColor: Theme
+                  .of(context)
+                  .scaffoldBackgroundColor,
+              closedColor: Theme
+                  .of(context)
+                  .colorScheme
+                  .primary,
+            ),
+          ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: taskService.tasks.isEmpty
+              ? Column(
+            children: <Widget>[
+              SizedBox(
+                height: windowHeight,
+                child: Center(
+                  child: CreateTask(),
+                ),
+              ),
+              SizedBox(
+                height: windowHeight,
+                child: Center(
+                  child: ImportTask(),
+                ),
+              ),
+            ],
+          )
+              : Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SizedBox(
+                  height: (() {
+                    if (shouldUseScreenHeight) {
+                      if (listViewShouldFillUp) {
+                        return windowHeight;
+                      }
+                    }
+
+                    return null;
+                  })(),
+                  child: Container(
+                    key: listViewKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(MEDIUM_SPACE),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "TASKS",
+                            style: getTitleTextStyle(context),
                           ),
-                        ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            padding: const EdgeInsets.only(top: MEDIUM_SPACE),
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: taskService.tasks.length,
+                            itemBuilder: (context, index) {
+                              final task = taskService.tasks[index];
+
+                              return TaskTile(
+                                task: task,
+                              );
+                            },
+                          ),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+                Container(
+                  height: shouldUseScreenHeight ? windowHeight : windowHeight - listViewHeight,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: MEDIUM_SPACE, vertical: LARGE_SPACE),
+                    child: Center(
+                      child: Paper(
+                        child: ImportTask(),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
