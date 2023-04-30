f782504de751365fdbb40032503ac8bf6fae5cf7
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-04-30 20:25:21 +0200

    refactor: Rewrite ImportTaskSheet.dart

--- lib/screens/ImportTaskSheet.dart
+++ lib/screens/ImportTaskSheet.dart
@@ -1,21 +1,29 @@
+import 'dart:convert';
+
 import 'package:enough_platform_widgets/enough_platform_widgets.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:locus/constants/spacing.dart';
 import 'package:locus/screens/import_task_sheet_widgets/ImportSelection.dart';
 import 'package:locus/screens/import_task_sheet_widgets/ImportSuccess.dart';
 import 'package:locus/screens/import_task_sheet_widgets/NameForm.dart';
+import 'package:locus/screens/import_task_sheet_widgets/ScanQRCode.dart';
 import 'package:locus/screens/import_task_sheet_widgets/URLForm.dart';
 import 'package:locus/screens/import_task_sheet_widgets/ViewImportOverview.dart';
 import 'package:locus/services/view_service.dart';
 import 'package:locus/utils/theme.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
 
+import '../services/task_service.dart';
 import '../widgets/ModalSheet.dart';
 
 enum ImportScreen {
   ask,
-  url,
-  name,
+  importFile,
+  askURL,
+  askName,
+  scanQR,
   present,
   error,
   done,
@@ -28,17 +36,20 @@ class ImportTaskSheet extends StatefulWidget {
   State<ImportTaskSheet> createState() => _ImportTaskSheetState();
 }
 
-class _ImportTaskSheetState extends State<ImportTaskSheet> with TickerProviderStateMixin {
+class _ImportTaskSheetState extends State<ImportTaskSheet>
+    with TickerProviderStateMixin {
   final _nameController = TextEditingController();
   ImportScreen _screen = ImportScreen.ask;
   String? errorMessage;
   TaskView? _taskView;
+  bool isLoading = false;
 
   void reset() {
     setState(() {
       _screen = ImportScreen.ask;
       errorMessage = null;
       _taskView = null;
+      isLoading = false;
     });
   }
 
@@ -60,6 +71,69 @@ class _ImportTaskSheetState extends State<ImportTaskSheet> with TickerProviderSt
     super.dispose();
   }
 
+  void _importFile() async {
+    final taskService = context.read<TaskService>();
+    final viewService = context.read<ViewService>();
+
+    FilePickerResult? result;
+
+    setState(() {
+      errorMessage = null;
+      isLoading = true;
+      _screen = ImportScreen.importFile;
+    });
+
+    try {
+      result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ["json"],
+        dialogTitle: "Select a viewkey file",
+        withData: true,
+      );
+    } catch (_) {
+      setState(() {
+        errorMessage = "An error occurred while importing the task.";
+      });
+    }
+
+    try {
+      if (result != null) {
+        final rawData = const Utf8Decoder().convert(result.files[0].bytes!);
+        final data = jsonDecode(rawData);
+
+        final taskView = TaskView(
+          relays: List<String>.from(data["relays"]),
+          nostrPublicKey: data["nostrPublicKey"],
+          signPublicKey: data["signPublicKey"],
+          viewPrivateKey: data["viewPrivateKey"],
+        );
+
+        final errorMessage = await taskView.validate(
+          taskService: taskService,
+          viewService: viewService,
+        );
+
+        if (errorMessage != null) {
+          setState(() {
+            this.errorMessage = errorMessage;
+          });
+
+          return;
+        } else {
+          setState(() {
+            _taskView = taskView;
+            _screen = ImportScreen.askName;
+          });
+        }
+      }
+    } catch (_) {
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -67,101 +141,127 @@ class _ImportTaskSheetState extends State<ImportTaskSheet> with TickerProviderSt
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        ModalSheet(
-          child: Padding(
-            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "Import a task",
-                  style: getSubTitleTextStyle(context),
-                ),
-                const SizedBox(height: LARGE_SPACE),
-                if (_screen == ImportScreen.ask)
-                  ImportSelection(
-                    onGoToURL: () => setState(() {
-                      _screen = ImportScreen.url;
-                    }),
-                    onTaskImported: (taskView) {
-                      setState(() {
-                        _taskView = taskView;
-                        _screen = ImportScreen.name;
-                      });
-                    },
-                    onTaskError: (message) {
-                      setState(() {
-                        errorMessage = message;
-                        _screen = ImportScreen.error;
-                      });
-                    },
-                  )
-                else if (_screen == ImportScreen.url)
-                  URLForm(
-                    onSubmitted: (taskView) {
-                      setState(() {
-                        _taskView = taskView;
-                        _screen = ImportScreen.name;
-                      });
-                    },
-                    onTaskError: (message) {
-                      setState(() {
-                        errorMessage = message;
-                        _screen = ImportScreen.error;
-                      });
-                    },
-                  )
-                else if (_screen == ImportScreen.name)
-                  NameForm(
-                    controller: _nameController,
-                    onSubmitted: () {
-                      _taskView!.update(name: _nameController.text);
-                      setState(() {
-                        _screen = ImportScreen.present;
-                      });
-                    },
-                  )
-                else if (_screen == ImportScreen.present)
-                  ViewImportOverview(
-                    view: _taskView!,
-                    onGoToNameEdit: () => setState(() {
-                      _screen = ImportScreen.name;
-                    }),
-                    onImport: importView,
-                  )
-                else if (_screen == ImportScreen.done)
-                  ImportSuccess(
-                    onClose: () => Navigator.of(context).pop(_taskView!),
-                  )
-                else if (_screen == ImportScreen.error)
-                  Column(
-                    children: <Widget>[
-                      Icon(context.platformIcons.error, size: 64, color: Colors.red),
-                      const SizedBox(height: MEDIUM_SPACE),
-                      Text(
-                        "An error occurred while importing the task",
-                        style: getSubTitleTextStyle(context),
-                      ),
-                      const SizedBox(height: SMALL_SPACE),
-                      Text(
-                        errorMessage!,
-                        style: getBodyTextTextStyle(context).copyWith(color: Colors.red),
-                      ),
-                      const SizedBox(height: LARGE_SPACE),
-                      PlatformElevatedButton(
-                        padding: const EdgeInsets.all(MEDIUM_SPACE),
-                        onPressed: reset,
-                        material: (_, __) => MaterialElevatedButtonData(
-                          icon: const Icon(Icons.arrow_back_rounded),
+        // QR Scanner needs full width and height
+        if (_screen == ImportScreen.scanQR)
+          ScanQRCode(
+            onAbort: reset,
+            onURLDetected: (url) {},
+          )
+        else
+          ModalSheet(
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: Column(
+                children: <Widget>[
+                  if (_screen == ImportScreen.ask)
+                    ImportSelection(
+                      onSelect: (type) {
+                        switch (type) {
+                          case ImportSelectionType.file:
+                            _importFile();
+                            break;
+                          case ImportSelectionType.url:
+                            setState(() {
+                              _screen = ImportScreen.askURL;
+                            });
+                            break;
+                          case ImportSelectionType.qr:
+                            setState(() {
+                              _screen = ImportScreen.scanQR;
+                            });
+                            break;
+                        }
+                      },
+                    )
+                  else if (_screen == ImportScreen.askURL)
+                    URLForm(
+                      onSubmitted: (taskView) {
+                        setState(() {
+                          _taskView = taskView;
+                          _screen = ImportScreen.askName;
+                        });
+                      },
+                      onTaskError: (message) {
+                        setState(() {
+                          errorMessage = message;
+                          _screen = ImportScreen.error;
+                        });
+                      },
+                    )
+                  else if (_screen == ImportScreen.askName)
+                    NameForm(
+                      controller: _nameController,
+                      onSubmitted: () {
+                        _taskView!.update(name: _nameController.text);
+                        setState(() {
+                          _screen = ImportScreen.present;
+                        });
+                      },
+                    )
+                  else if (_screen == ImportScreen.importFile)
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          "Importing task...",
+                          style: getSubTitleTextStyle(context),
                         ),
-                        child: const Text("Go back"),
-                      ),
-                    ],
-                  ),
-                const SizedBox(height: LARGE_SPACE),
-              ],
+                        const SizedBox(height: SMALL_SPACE),
+                        if (isLoading)
+                          const CircularProgressIndicator()
+                        else if (errorMessage != null)
+                          Text(
+                            errorMessage!,
+                            style: getBodyTextTextStyle(context)
+                                .copyWith(color: Colors.red),
+                          ),
+                      ],
+                    )
+                  else if (_screen == ImportScreen.present)
+                    ViewImportOverview(
+                      view: _taskView!,
+                      onGoToNameEdit: () => setState(() {
+                        _screen = ImportScreen.askName;
+                      }),
+                      onImport: importView,
+                    )
+                  else if (_screen == ImportScreen.done)
+                    ImportSuccess(
+                      onClose: () => Navigator.of(context).pop(_taskView!),
+                    )
+                  else if (_screen == ImportScreen.error)
+                    Column(
+                      children: <Widget>[
+                        Icon(context.platformIcons.error,
+                            size: 64, color: Colors.red),
+                        const SizedBox(height: MEDIUM_SPACE),
+                        Text(
+                          "An error occurred while importing the task",
+                          style: getSubTitleTextStyle(context),
+                        ),
+                        const SizedBox(height: SMALL_SPACE),
+                        Text(
+                          errorMessage!,
+                          style: getBodyTextTextStyle(context)
+                              .copyWith(color: Colors.red),
+                        ),
+                        const SizedBox(height: LARGE_SPACE),
+                        PlatformElevatedButton(
+                          padding: const EdgeInsets.all(MEDIUM_SPACE),
+                          onPressed: reset,
+                          material: (_, __) => MaterialElevatedButtonData(
+                            icon: const Icon(Icons.arrow_back_rounded),
+                          ),
+                          child: const Text("Go back"),
+                        ),
+                      ],
+                    ),
+                  const SizedBox(height: LARGE_SPACE),
+                ],
+              ),
             ),
           ),
-        ),
       ],
     );
   }
--- lib/screens/import_task_sheet_widgets/ImportSelection.dart
+++ lib/screens/import_task_sheet_widgets/ImportSelection.dart
@@ -1,25 +1,30 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:enough_platform_widgets/enough_platform_widgets.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:locus/services/view_service.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
 
 import '../../constants/spacing.dart';
 import '../../services/task_service.dart';
 import '../../utils/theme.dart';
 
+enum ImportSelectionType {
+  url,
+  file,
+  qr,
+}
+
 class ImportSelection extends StatefulWidget {
-  final void Function() onGoToURL;
-  final void Function(TaskView) onTaskImported;
-  final void Function(String) onTaskError;
+  final void Function(ImportSelectionType) onSelect;
 
   const ImportSelection({
-    required this.onGoToURL,
-    required this.onTaskImported,
-    required this.onTaskError,
-    Key? key,}) : super(key: key);
+    required this.onSelect,
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<ImportSelection> createState() => _ImportSelectionState();
@@ -30,11 +35,13 @@ class _ImportSelectionState extends State<ImportSelection> {
 
   @override
   Widget build(BuildContext context) {
-    final taskService = context.read<TaskService>();
-    final viewService = context.read<ViewService>();
-
     return Column(
       children: <Widget>[
+        Text(
+          "Import a task",
+          style: getSubTitleTextStyle(context),
+        ),
+        const SizedBox(height: LARGE_SPACE),
         Text(
           "How would you like to import?",
           style: getBodyTextTextStyle(context),
@@ -47,81 +54,40 @@ class _ImportSelectionState extends State<ImportSelection> {
           ),
           const SizedBox(height: MEDIUM_SPACE),
         ],
-        Row(
+        Wrap(
+          spacing: MEDIUM_SPACE,
+          direction: Axis.vertical,
+          crossAxisAlignment: WrapCrossAlignment.center,
           children: <Widget>[
-            Expanded(
-              child: PlatformElevatedButton(
-                padding: const EdgeInsets.all(MEDIUM_SPACE),
-                onPressed: () {
-                  widget.onGoToURL();
-                },
-                material: (_, __) =>
-                    MaterialElevatedButtonData(
-                      icon: const Icon(Icons.link_rounded),
-                    ),
-                child: const Text("Import URL"),
+            PlatformElevatedButton(
+              padding: const EdgeInsets.all(MEDIUM_SPACE),
+              onPressed: () {
+                widget.onSelect(ImportSelectionType.url);
+              },
+              material: (_, __) => MaterialElevatedButtonData(
+                icon: const Icon(Icons.link_rounded),
               ),
+              child: const Text("Import URL"),
             ),
-            const SizedBox(width: MEDIUM_SPACE),
-            Expanded(
-              child: PlatformElevatedButton(
-                padding: const EdgeInsets.all(MEDIUM_SPACE),
-                material: (_, __) =>
-                    MaterialElevatedButtonData(
-                      icon: const Icon(Icons.file_open_rounded),
-                    ),
-                onPressed: () async {
-                  FilePickerResult? result;
-
-                  setState(() {
-                    errorMessage = null;
-                  });
-
-                  try {
-                    result = await FilePicker.platform.pickFiles(
-                      type: FileType.custom,
-                      allowedExtensions: ["json"],
-                      dialogTitle: "Select a viewkey file",
-                      withData: true,
-                    );
-                  } catch (_) {
-                    setState(() {
-                      errorMessage = "An error occurred while importing the task.";
-                    });
-                  }
-
-                  try {
-                    if (result != null) {
-                      final rawData = const Utf8Decoder().convert(result.files[0].bytes!);
-                      final data = jsonDecode(rawData);
-
-                      final taskView = TaskView(
-                        relays: List<String>.from(data["relays"]),
-                        nostrPublicKey: data["nostrPublicKey"],
-                        signPublicKey: data["signPublicKey"],
-                        viewPrivateKey: data["viewPrivateKey"],
-                      );
-
-                      final errorMessage = await taskView.validate(
-                        taskService: taskService,
-                        viewService: viewService,
-                      );
-
-                      if (errorMessage != null) {
-                        widget.onTaskError(errorMessage);
-                        return;
-                      } else {
-                        widget.onTaskImported(taskView);
-                      }
-                    }
-                  } catch (_) {
-                    setState(() {
-                      errorMessage = "This does not seem to be a valid viewkey file.";
-                    });
-                  }
-                },
-                child: const Text("Import file"),
+            PlatformElevatedButton(
+              padding: const EdgeInsets.all(MEDIUM_SPACE),
+              material: (_, __) => MaterialElevatedButtonData(
+                icon: const Icon(Icons.file_open_rounded),
               ),
+              onPressed: () async {
+                widget.onSelect(ImportSelectionType.file);
+              },
+              child: const Text("Import file"),
+            ),
+            PlatformElevatedButton(
+              padding: const EdgeInsets.all(MEDIUM_SPACE),
+              material: (_, __) => MaterialElevatedButtonData(
+                icon: const Icon(Icons.qr_code_scanner_rounded),
+              ),
+              onPressed: () {
+                widget.onSelect(ImportSelectionType.qr);
+              },
+              child: Text("Scan QR code"),
             ),
           ],
         ),
