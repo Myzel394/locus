e02ec9bf4129c4e01041dd37fdc57b02e68f8fcb
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-05-01 15:51:19 +0200

    refactor: Use general LocationsMap

--- lib/widgets/LocationsMap.dart
+++ lib/widgets/LocationsMap.dart
@@ -20,13 +20,14 @@ class LocationsMapController extends ChangeNotifier {
   // To inform our wrappers to update the map, we use a stream.
   // This emits event to which our wrappers listen to.
   final StreamController<Map<String, dynamic>> _eventEmitter =
-      StreamController.broadcast();
+  StreamController.broadcast();
 
   LocationsMapController({
     List<LocationPointService>? locations,
   }) : _locations = locations ?? [];
 
-  static DateTime normalizeDateTime(final DateTime dateTime) => DateTime(
+  static DateTime normalizeDateTime(final DateTime dateTime) =>
+      DateTime(
         dateTime.year,
         dateTime.month,
         dateTime.day,
@@ -71,7 +72,7 @@ class LocationsMapController extends ChangeNotifier {
   Map<DateTime, List<LocationPointService>> getLocationsPerHour() =>
       locations.fold(
         {},
-        (final Map<DateTime, List<LocationPointService>> value, element) {
+            (final Map<DateTime, List<LocationPointService>> value, element) {
           final date = normalizeDateTime(element.createdAt);
 
           if (value.containsKey(date)) {
@@ -105,6 +106,7 @@ class LocationsMapAppleMaps extends StatefulWidget {
 }
 
 class _LocationsMapAppleMapsState extends State<LocationsMapAppleMaps> {
+  late final StreamSubscription _controllerSubscription;
   AppleMaps.AppleMapController? _controller;
   Position? userPosition;
 
@@ -113,7 +115,8 @@ class _LocationsMapAppleMapsState extends State<LocationsMapAppleMaps> {
     super.initState();
 
     widget.controller.addListener(rebuild);
-    widget.controller.eventListener.listen(eventEmitterListener);
+    _controllerSubscription =
+        widget.controller.eventListener.listen(eventEmitterListener);
 
     fetchInitialPosition();
   }
@@ -121,6 +124,7 @@ class _LocationsMapAppleMapsState extends State<LocationsMapAppleMaps> {
   @override
   void dispose() {
     widget.controller.removeListener(rebuild);
+    _controllerSubscription.cancel();
 
     super.dispose();
   }
@@ -214,24 +218,26 @@ class _LocationsMapAppleMapsState extends State<LocationsMapAppleMaps> {
       myLocationButtonEnabled: true,
       annotations: widget.controller.locations.isNotEmpty
           ? {
-              AppleMaps.Annotation(
-                annotationId: AppleMaps.AnnotationId(
-                  "annotation_${widget.controller.locations.last.latitude}:${widget.controller.locations.last.longitude}",
-                ),
-                position: AppleMaps.LatLng(
-                  widget.controller.locations.last.latitude,
-                  widget.controller.locations.last.longitude,
-                ),
-                infoWindow: AppleMaps.InfoWindow(
-                  title: "Last location",
-                  snippet: snippetText,
-                ),
-              ),
-            }
+        AppleMaps.Annotation(
+          annotationId: AppleMaps.AnnotationId(
+            "annotation_${widget.controller.locations.last.latitude}:${widget
+                .controller.locations.last.longitude}",
+          ),
+          position: AppleMaps.LatLng(
+            widget.controller.locations.last.latitude,
+            widget.controller.locations.last.longitude,
+          ),
+          infoWindow: AppleMaps.InfoWindow(
+            title: "Last location",
+            snippet: snippetText,
+          ),
+        ),
+      }
           : {},
       circles: widget.controller.locations
           .map(
-            (location) => AppleMaps.Circle(
+            (location) =>
+            AppleMaps.Circle(
               circleId: AppleMaps.CircleId(
                 "circle_${location.latitude}:${location.longitude}",
               ),
@@ -244,7 +250,7 @@ class _LocationsMapAppleMapsState extends State<LocationsMapAppleMaps> {
               strokeWidth: location.accuracy < 10 ? 1 : 3,
               radius: location.accuracy,
             ),
-          )
+      )
           .toSet(),
     );
   }
@@ -264,6 +270,7 @@ class LocationsMapOSM extends StatefulWidget {
 
 class _LocationsMapOSMState extends State<LocationsMapOSM> {
   late final MapController _controller;
+  late final StreamSubscription _controllerSubscription;
 
   @override
   void initState() {
@@ -272,20 +279,42 @@ class _LocationsMapOSMState extends State<LocationsMapOSM> {
     _controller = MapController(
       initMapWithUserPosition: true,
     );
-    widget.controller.addListener(drawCircles);
+    widget.controller.addListener(rebuild);
+    _controllerSubscription =
+        widget.controller.eventListener.listen(eventEmitterListener);
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    widget.controller.removeListener(drawCircles);
+    widget.controller.removeListener(rebuild);
+    _controllerSubscription.cancel();
 
     super.dispose();
   }
 
   void rebuild() {
+    drawCircles();
+
     setState(() {});
   }
+
+  void eventEmitterListener(final Map<String, dynamic> data) async {
+    switch (data["type"]) {
+      case "goTo":
+        final location = data["location"] as LocationPointService;
+
+        _controller.goToLocation(
+          GeoPoint(
+            latitude: location.latitude,
+            longitude: location.longitude,
+          ),
+        );
+
+        break;
+    }
+  }
+
 
   void drawCircles() {
     _controller.removeAllCircle();
