bdca957b1d20fd58a1c8d5ed0868e8b706aa89a1
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-05-25 12:19:23 +0200

    feat: Add log models

--- lib/models/log.dart
+++ lib/models/log.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:hive/hive.dart';
+import 'package:locus/services/task_service.dart';
 import 'package:uuid/uuid.dart';
 
 part 'log.g.dart';
@@ -66,4 +69,270 @@ class Log extends HiveObject {
         initiator: initiator,
         payload: payload,
       );
+
+  factory Log.createTask({
+    required LogInitiator initiator,
+    required String taskId,
+    required String taskName,
+  }) =>
+      Log.create(
+        type: LogType.taskCreated,
+        initiator: initiator,
+        payload: jsonEncode(
+          CreateTaskData(
+            id: taskId,
+            name: taskName,
+          ).toJSON(),
+        ),
+      );
+
+  CreateTaskData get createTaskData {
+    if (type != LogType.taskCreated) {
+      throw Exception("Log is not of type taskCreated");
+    }
+    return CreateTaskData.fromJSON(jsonDecode(payload));
+  }
+
+  factory Log.deleteTask({
+    required LogInitiator initiator,
+    // No reference to the id, since its deleted
+    required String taskName,
+  }) =>
+      Log.create(
+        type: LogType.taskDeleted,
+        initiator: initiator,
+        payload: jsonEncode(
+          DeleteTaskData(
+            name: taskName,
+          ).toJSON(),
+        ),
+      );
+
+  DeleteTaskData get deleteTaskData {
+    if (type != LogType.taskDeleted) {
+      throw Exception("Log is not of type taskDeleted");
+    }
+    return DeleteTaskData.fromJSON(jsonDecode(payload));
+  }
+
+  factory Log.startTask({
+    required LogInitiator initiator,
+    required String taskId,
+    required String taskName,
+  }) =>
+      Log.create(
+        type: LogType.taskStarted,
+        initiator: initiator,
+        payload: jsonEncode(
+          StartTaskData(
+            id: taskId,
+            name: taskName,
+          ).toJSON(),
+        ),
+      );
+
+  StartTaskData get startTaskData {
+    if (type != LogType.taskStarted) {
+      throw Exception("Log is not of type taskStarted");
+    }
+    return StartTaskData.fromJSON(jsonDecode(payload));
+  }
+
+  factory Log.stopTask({
+    required LogInitiator initiator,
+    required String taskId,
+    required String taskName,
+  }) =>
+      Log.create(
+        type: LogType.taskStopped,
+        initiator: initiator,
+        payload: jsonEncode(
+          StopTaskData(
+            id: taskId,
+            name: taskName,
+          ).toJSON(),
+        ),
+      );
+
+  StopTaskData get stopTaskData {
+    if (type != LogType.taskStopped) {
+      throw Exception("Log is not of type taskStopped");
+    }
+    return StopTaskData.fromJSON(jsonDecode(payload));
+  }
+
+  factory Log.updateLocation({
+    required LogInitiator initiator,
+    required String taskId,
+    required String taskName,
+    required double latitude,
+    required double longitude,
+    required double accuracy,
+    required List<UpdatedTaskData> tasks,
+  }) =>
+      Log.create(
+        type: LogType.updatedLocation,
+        initiator: initiator,
+        payload: jsonEncode(
+          UpdateLocationData(
+            latitude: latitude,
+            longitude: longitude,
+            accuracy: accuracy,
+            tasks: tasks,
+          ).toJSON(),
+        ),
+      );
+
+  UpdateLocationData get updateLocationData {
+    if (type != LogType.updatedLocation) {
+      throw Exception("Log is not of type updatedLocation");
+    }
+    return UpdateLocationData.fromJSON(jsonDecode(payload));
+  }
+}
+
+class UpdatedTaskData {
+  final String id;
+  final String name;
+
+  const UpdatedTaskData({
+    required this.id,
+    required this.name,
+  });
+}
+
+class UpdateLocationData {
+  final double latitude;
+  final double longitude;
+  final double accuracy;
+  final List<UpdatedTaskData> tasks;
+
+  const UpdateLocationData({
+    required this.latitude,
+    required this.longitude,
+    required this.accuracy,
+    required this.tasks,
+  });
+
+  factory UpdateLocationData.fromJSON(Map<String, dynamic> json) =>
+      UpdateLocationData(
+        latitude: json["a"],
+        longitude: json["o"],
+        accuracy: json["c"],
+        tasks: List<UpdatedTaskData>.from(
+          List<Map<String, String>>.from(json["t"]).map(
+                (task) =>
+                UpdatedTaskData(
+                  id: task["i"]!,
+                  name: task["n"]!,
+                ),
+          ),
+        ),
+      );
+
+  Map<String, dynamic> toJSON() =>
+      {
+        "o": latitude,
+        "a": longitude,
+        "c": accuracy,
+        "t": List<Map<String, String>>.from(
+          tasks.map(
+                (task) =>
+            {
+              "i": task.id,
+              "n": task.name,
+            },
+          ),
+        ),
+      };
+}
+
+class CreateTaskData {
+  final String id;
+  final String name;
+
+  const CreateTaskData({
+    required this.id,
+    required this.name,
+  });
+
+  factory CreateTaskData.fromJSON(Map<String, dynamic> json) =>
+      CreateTaskData(
+        id: json["i"],
+        name: json["n"],
+      );
+
+  Map<String, dynamic> toJSON() =>
+      {
+        "i": id,
+        "n": name,
+      };
+
+  Task getTask(final TaskService taskService) => taskService.getByID(id);
+}
+
+class DeleteTaskData {
+  final String name;
+
+  const DeleteTaskData({
+    required this.name,
+  });
+
+  factory DeleteTaskData.fromJSON(Map<String, dynamic> json) =>
+      DeleteTaskData(
+        name: json["n"],
+      );
+
+  Map<String, dynamic> toJSON() =>
+      {
+        "n": name,
+      };
+}
+
+class StartTaskData {
+  final String id;
+  final String name;
+
+  const StartTaskData({
+    required this.id,
+    required this.name,
+  });
+
+  factory StartTaskData.fromJSON(Map<String, dynamic> json) =>
+      StartTaskData(
+        id: json["i"],
+        name: json["n"],
+      );
+
+  Map<String, dynamic> toJSON() =>
+      {
+        "i": id,
+        "n": name,
+      };
+
+  Task getTask(final TaskService taskService) => taskService.getByID(id);
+}
+
+class StopTaskData {
+  final String id;
+  final String name;
+
+  const StopTaskData({
+    required this.id,
+    required this.name,
+  });
+
+  factory StopTaskData.fromJSON(Map<String, dynamic> json) =>
+      StopTaskData(
+        id: json["i"],
+        name: json["n"],
+      );
+
+  Map<String, dynamic> toJSON() =>
+      {
+        "i": id,
+        "n": name,
+      };
+
+  Task getTask(final TaskService taskService) => taskService.getByID(id);
 }
