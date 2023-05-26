4281d7ac9c886e8547f350744e66c0a61ba6b836
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-05-26 22:58:07 +0200

    feat: Add logs info

--- lib/screens/LogsScreen.dart
+++ lib/screens/LogsScreen.dart
@@ -6,6 +6,7 @@ import 'package:locus/constants/spacing.dart';
 import 'package:locus/screens/LogDetailScreen.dart';
 import 'package:locus/screens/log_detail_screen_widgets/LogCreatedAtInfo.dart';
 import 'package:locus/screens/log_detail_screen_widgets/LogTypeInfo.dart';
+import 'package:locus/utils/theme.dart';
 import 'package:provider/provider.dart';
 
 import '../models/log.dart';
@@ -28,90 +29,125 @@ class _LogsScreenState extends State<LogsScreen> {
 
     return PlatformScaffold(
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: logService.logs.length,
-          itemBuilder: (context, final int index) {
-            // Reverse
-            final Log log = logService.logs[logService.logs.length - index - 1];
-            return Stack(
-              children: <Widget>[
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  top: 0,
-                  bottom: 0,
-                  child: Hero(
-                    tag: "${log.id}:paper",
-                    child: Material(
-                      color: Colors.transparent,
-                      child: Paper(
-                        roundness: 0,
-                        child: Container(),
-                      ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: MEDIUM_SPACE, vertical: LARGE_SPACE),
+                child: Paper(
+                  child: Padding(
+                    padding: const EdgeInsets.all(MEDIUM_SPACE),
+                    child: Column(
+                      children: <Widget>[
+                        Icon(
+                          context.platformIcons.info,
+                          size: 38,
+                        ),
+                        const SizedBox(height: MEDIUM_SPACE),
+                        Text(
+                          l10n.logs_title,
+                          style: getTitle2TextStyle(context),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: MEDIUM_SPACE),
+                        Text(
+                          l10n.logs_description,
+                          style: getBodyTextTextStyle(context),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                PlatformListTile(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      PageRouteBuilder(
-                        pageBuilder: (_, __, ___) => LogDetailScreen(log: log),
-                        fullscreenDialog: true,
-                        barrierColor: Colors.black.withOpacity(.3),
-                        opaque: false,
-                        barrierDismissible: true,
-                      ),
-                    );
-                  },
-                  leading: Hero(
-                    tag: "${log.id}:icon",
-                    child: LogIcon(log: log),
-                  ),
-                  title: Hero(
-                    tag: "${log.id}:title",
-                    child: Material(
-                      color: Colors.transparent,
-                      child: Text(
-                        log.getTitle(context),
-                      ),
-                    ),
-                  ),
-                  subtitle: Row(
+              ),
+              ListView.builder(
+                itemCount: logService.logs.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, final int index) {
+                  // Reverse
+                  final Log log = logService.logs[logService.logs.length - index - 1];
+                  return Stack(
                     children: <Widget>[
-                      Hero(
-                        tag: "${log.id}:type",
-                        child: Material(
-                          color: Colors.transparent,
-                          child: LogTypeInfo(log: log),
-                        ),
-                      ),
-                      const SizedBox(width: SMALL_SPACE),
-                      Hero(
-                        tag: "${log.id}:createdAt",
-                        child: Material(
-                          color: Colors.transparent,
-                          child: LogCreatedInfo(log: log),
-                        ),
-                      ),
-                    ],
-                  ),
-                  trailing: log.initiator == LogInitiator.system
-                      ? Hero(
-                          tag: "${log.id}:initiator",
-                          child: PlatformWidget(
-                            material: (_, __) => const Icon(
-                              Icons.laptop,
-                            ),
-                            cupertino: (_, __) => const Icon(
-                              CupertinoIcons.bolt,
+                      Positioned(
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0,
+                        child: Hero(
+                          tag: "${log.id}:paper",
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Paper(
+                              roundness: 0,
+                              child: Container(),
                             ),
                           ),
-                        )
-                      : null,
-                ),
-              ],
-            );
-          },
+                        ),
+                      ),
+                      PlatformListTile(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            PageRouteBuilder(
+                              pageBuilder: (_, __, ___) => LogDetailScreen(log: log),
+                              fullscreenDialog: true,
+                              barrierColor: Colors.black.withOpacity(.3),
+                              opaque: false,
+                              barrierDismissible: true,
+                            ),
+                          );
+                        },
+                        leading: Hero(
+                          tag: "${log.id}:icon",
+                          child: LogIcon(log: log),
+                        ),
+                        title: Hero(
+                          tag: "${log.id}:title",
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Text(
+                              log.getTitle(context),
+                            ),
+                          ),
+                        ),
+                        subtitle: Row(
+                          children: <Widget>[
+                            Hero(
+                              tag: "${log.id}:type",
+                              child: Material(
+                                color: Colors.transparent,
+                                child: LogTypeInfo(log: log),
+                              ),
+                            ),
+                            const SizedBox(width: SMALL_SPACE),
+                            Hero(
+                              tag: "${log.id}:createdAt",
+                              child: Material(
+                                color: Colors.transparent,
+                                child: LogCreatedInfo(log: log),
+                              ),
+                            ),
+                          ],
+                        ),
+                        trailing: log.initiator == LogInitiator.system
+                            ? Hero(
+                                tag: "${log.id}:initiator",
+                                child: PlatformWidget(
+                                  material: (_, __) => const Icon(
+                                    Icons.laptop,
+                                  ),
+                                  cupertino: (_, __) => const Icon(
+                                    CupertinoIcons.bolt,
+                                  ),
+                                ),
+                              )
+                            : null,
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
