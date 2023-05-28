33bf117c55a49705f313ed6e67bcb05ef3ee5708
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-05-28 19:33:55 +0200

    ui: Add PINView.dart animation

--- lib/widgets/PINView.dart
+++ lib/widgets/PINView.dart
@@ -1,4 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:locus/constants/spacing.dart';
 import 'package:locus/utils/theme.dart';
 import 'package:locus/widgets/Paper.dart';
@@ -13,20 +15,33 @@ class PINView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final middleIndex = pin.toString().length ~/ 2;
+
     return Wrap(
       direction: Axis.horizontal,
       spacing: SMALL_SPACE,
       children: pin
           .toString()
           .split("")
-          .map(
-            (digit) => Paper(
+          .mapIndexed(
+            (index, digit) => Paper(
               width: null,
               child: Text(
                 digit,
                 style: getTitle2TextStyle(context),
               ),
-            ),
+            )
+                .animate()
+                .then(delay: Duration(milliseconds: (index - middleIndex).abs() * 400))
+                .moveX(
+                  begin: (index - middleIndex) * 10,
+                  end: 0,
+                  duration: 800.ms,
+                  curve: Curves.easeOut,
+                )
+                .fadeIn(
+                  duration: 800.ms,
+                ),
           )
           .toList(),
     );
