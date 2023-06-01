5602d2b2a6f1e400970a6c160ce235fe3db278e0
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-01 11:23:55 +0200

    current stand

--- lib/api/get-locations.dart
+++ lib/api/get-locations.dart
@@ -1,34 +1,22 @@
 import 'dart:io';
 
-import 'package:basic_utils/basic_utils.dart';
 import 'package:cryptography/cryptography.dart';
 import 'package:locus/services/location_point_service.dart';
 import 'package:nostr/nostr.dart';
 
-Future<void Function()> getLocations({
-  required final String nostrPublicKey,
+Future<WebSocket> openSocket({
+  required final String url,
+  required final Request request,
   required final SecretKey encryptionPassword,
-  required final List<String> relays,
   required void Function(LocationPointService) onLocationFetched,
   required void Function() onEnd,
-  bool onlyLatestPosition = false,
-  DateTime? from,
 }) async {
-  final request = Request(generate64RandomHexChars(), [
-    Filter(
-      kinds: [1000],
-      authors: [nostrPublicKey],
-      limit: onlyLatestPosition ? 1 : null,
-      since: from == null ? null : (from.millisecondsSinceEpoch / 1000).floor(),
-    ),
-  ]);
-
-  final socket = await WebSocket.connect(
-    relays.first,
-  );
   final List<Future<LocationPointService>> decryptionProcesses = [];
-  bool hasReceivedEndOfStream = false;
+
   bool hasReceivedEvent = false;
+  bool hasReceivedEndOfStream = false;
+
+  final socket = await WebSocket.connect(url);
 
   socket.add(request.serialize());
 
@@ -61,14 +49,62 @@ Future<void Function()> getLocations({
 
         hasReceivedEndOfStream = true;
 
-        if ((decryptionProcesses.isEmpty && hasReceivedEvent) || !hasReceivedEvent) {
+        if ((decryptionProcesses.isEmpty && hasReceivedEvent) ||
+            !hasReceivedEvent) {
           onEnd();
         }
         break;
     }
   });
 
-  return () {
-    socket.close();
+  return socket;
+}
+
+Future<void Function()> getLocations({
+  required final String nostrPublicKey,
+  required final SecretKey encryptionPassword,
+  required final List<String> relays,
+  required void Function(LocationPointService) onLocationFetched,
+  required void Function() onEnd,
+  bool onlyLatestPosition = false,
+  DateTime? from,
+}) async {
+  final request = Request(generate64RandomHexChars(), [
+    Filter(
+      kinds: [1000],
+      authors: [nostrPublicKey],
+      limit: onlyLatestPosition ? 1 : null,
+      since: from == null ? null : (from.millisecondsSinceEpoch / 1000).floor(),
+    ),
+  ]);
+
+  final List<String> existingIDs = [];
+
+  final List<WebSocket> socketProcesses = [];
+
+  for (final relay in relays) {
+    final socketProcess = openSocket(
+      url: relay,
+      request: request,
+      encryptionPassword: encryptionPassword,
+      onLocationFetched: (final LocationPointService location) {
+        if (existingIDs.contains(location.id)) {
+          return;
+        }
+
+        existingIDs.add(location.id);
+
+        onLocationFetched(location);
+      },
+      onEnd: () {},
+    )..then(socketProcesses.add);
+  }
+
+  return () async {
+    for (final socketProcess in socketProcesses) {
+      final socket = await socketProcess;
+
+      socket.close();
+    }
   };
 }
