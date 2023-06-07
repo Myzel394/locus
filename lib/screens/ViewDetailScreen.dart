15f945c8b7afd4bef5f6aaf9702ffe57db104f97
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-07 13:30:52 +0200

    feat: Add bunny

--- lib/screens/ViewDetailScreen.dart
+++ lib/screens/ViewDetailScreen.dart
@@ -5,6 +5,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart'
     hide PlatformListTile;
 import 'package:locus/services/view_service.dart';
+import 'package:locus/utils/bunny.dart';
+import 'package:locus/widgets/EmptyLocationsThresholdScreen.dart';
 import 'package:locus/widgets/FillUpPaint.dart';
 import 'package:locus/widgets/LocationFetchError.dart';
 import 'package:locus/widgets/LocationsMap.dart';
@@ -33,16 +35,15 @@ class LineSliderTickMarkShape extends SliderTickMarkShape {
   }
 
   @override
-  void paint(
-    PaintingContext context,
-    Offset center, {
-    required RenderBox parentBox,
-    required SliderThemeData sliderTheme,
-    required Animation<double> enableAnimation,
-    required Offset thumbCenter,
-    required bool isEnabled,
-    required TextDirection textDirection,
-  }) {
+  void paint(PaintingContext context,
+      Offset center, {
+        required RenderBox parentBox,
+        required SliderThemeData sliderTheme,
+        required Animation<double> enableAnimation,
+        required Offset thumbCenter,
+        required bool isEnabled,
+        required TextDirection textDirection,
+      }) {
     // This block is just copied from `slider_theme`
     final bool isTickMarkRightOfThumb = center.dx > thumbCenter.dx;
     final begin = isTickMarkRightOfThumb
@@ -90,21 +91,12 @@ class _ViewDetailScreenState extends State<ViewDetailScreen> {
   void initState() {
     super.initState();
 
-    addListener();
-  }
+    emptyLocationsCount++;
 
-  @override
-  void dispose() {
-    _unsubscribeGetLocations?.call();
-    _controller.dispose();
-
-    super.dispose();
-  }
-
-  addListener() {
     _unsubscribeGetLocations = widget.view.getLocations(
       from: DateTime.now().subtract(1.days),
       onLocationFetched: (final LocationPointService location) {
+        emptyLocationsCount = 0;
         if (!mounted) {
           return;
         }
@@ -125,15 +117,23 @@ class _ViewDetailScreenState extends State<ViewDetailScreen> {
   }
 
   @override
+  void dispose() {
+    _unsubscribeGetLocations?.call();
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
     final locationsPerHour = _controller.getLocationsPerHour();
     final maxLocations = locationsPerHour.values.isEmpty
         ? 0
         : locationsPerHour.values.fold(
-            0,
+        0,
             (value, element) =>
-                value > element.length ? value : element.length);
+        value > element.length ? value : element.length);
     final shades = getPrimaryColorShades(context);
 
     return PlatformScaffold(
@@ -141,113 +141,137 @@ class _ViewDetailScreenState extends State<ViewDetailScreen> {
         title: Text(l10n.viewDetails_title),
         trailingActions: _controller.locations.isNotEmpty
             ? <Widget>[
-                PlatformPopup<String>(
-                  type: PlatformPopupType.tap,
-                  items: [
-                    PlatformPopupMenuItem(
-                      label: PlatformListTile(
-                        leading: Icon(context.platformIcons.location),
-                        trailing: const SizedBox.shrink(),
-                        title:
-                            Text(l10n.viewDetails_actions_openLatestLocation),
-                      ),
-                      onPressed: () async {
-                        await showPlatformModalSheet(
-                          context: context,
-                          material: MaterialModalSheetData(),
-                          builder: (context) => OpenInMaps(
-                            destination: Coords(
-                                _controller.locations.last.latitude,
-                                _controller.locations.last.longitude),
-                          ),
-                        );
-                      },
-                    )
-                  ],
+          PlatformPopup<String>(
+            type: PlatformPopupType.tap,
+            items: [
+              PlatformPopupMenuItem(
+                label: PlatformListTile(
+                  leading: Icon(context.platformIcons.location),
+                  trailing: const SizedBox.shrink(),
+                  title:
+                  Text(l10n.viewDetails_actions_openLatestLocation),
                 ),
-              ]
+                onPressed: () async {
+                  await showPlatformModalSheet(
+                    context: context,
+                    material: MaterialModalSheetData(),
+                    builder: (context) =>
+                        OpenInMaps(
+                          destination: Coords(
+                              _controller.locations.last.latitude,
+                              _controller.locations.last.longitude),
+                        ),
+                  );
+                },
+              )
+            ],
+          ),
+        ]
             : [],
-        material: (_, __) => MaterialAppBarData(
-          centerTitle: true,
-        ),
-        cupertino: (_, __) => CupertinoNavigationBarData(
-          backgroundColor:
-              CupertinoTheme.of(context).barBackgroundColor.withOpacity(.5),
-        ),
+        material: (_, __) =>
+            MaterialAppBarData(
+              centerTitle: true,
+            ),
+        cupertino: (_, __) =>
+            CupertinoNavigationBarData(
+              backgroundColor:
+              CupertinoTheme
+                  .of(context)
+                  .barBackgroundColor
+                  .withOpacity(.5),
+            ),
       ),
-      body: _isError
-          ? const LocationFetchError()
-          : _isLoading
-              ? SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(MEDIUM_SPACE),
-                    child: LocationsLoadingScreen(
-                      locations: _controller.locations,
-                      onTimeout: () {
-                        setState(() {
-                          _isError = true;
-                        });
-                      },
+      body: (() {
+        if (_isError) {
+          return const LocationFetchError();
+        }
+
+        if (_isLoading) {
+          return SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(MEDIUM_SPACE),
+              child: LocationsLoadingScreen(
+                locations: _controller.locations,
+                onTimeout: () {
+                  setState(() {
+                    _isError = true;
+                  });
+                },
+              ),
+            ),
+          );
+        }
+
+        if (emptyLocationsCount > EMPTY_LOCATION_THRESHOLD) {
+          return const EmptyLocationsThresholdScreen();
+        }
+
+        return Column(
+          children: <Widget>[
+            Expanded(
+              flex: 11,
+              child: LocationsMap(
+                controller: _controller,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(24, (index) => 23 - index)
+                    .map((hour) {
+                  final date =
+                  DateTime.now().subtract(Duration(hours: hour));
+                  final normalizedDate =
+                  LocationsMapController.normalizeDateTime(date);
+
+                  final onTap = () {
+                    _controller.clear();
+
+                    final locations =
+                        locationsPerHour[normalizedDate] ?? [];
+
+                    if (locations.isNotEmpty) {
+                      _controller.addAll(locations);
+                      _controller.goTo(locations.last);
+                    }
+                  };
+                  final child = FillUpPaint(
+                    color: shades[0]!,
+                    fillPercentage:
+                    (locationsPerHour[normalizedDate]?.length ?? 0)
+                        .toDouble() /
+                        maxLocations,
+                    size: Size(
+                      MediaQuery
+                          .of(context)
+                          .size
+                          .width / 24,
+                      MediaQuery
+                          .of(context)
+                          .size
+                          .height * (1 / 12),
                     ),
-                  ),
-                )
-              : Column(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 11,
-                      child: LocationsMap(
-                        controller: _controller,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: List.generate(24, (index) => 23 - index)
-                            .map((hour) {
-                          final date =
-                              DateTime.now().subtract(Duration(hours: hour));
-                          final normalizedDate =
-                              LocationsMapController.normalizeDateTime(date);
+                  );
 
-                          final onTap = () {
-                            _controller.clear();
-
-                            final locations =
-                                locationsPerHour[normalizedDate] ?? [];
-
-                            if (locations.isNotEmpty) {
-                              _controller.addAll(locations);
-                              _controller.goTo(locations.last);
-                            }
-                          };
-                          final child = FillUpPaint(
-                            color: shades[0]!,
-                            fillPercentage:
-                                (locationsPerHour[normalizedDate]?.length ?? 0)
-                                        .toDouble() /
-                                    maxLocations,
-                            size: Size(
-                              MediaQuery.of(context).size.width / 24,
-                              MediaQuery.of(context).size.height * (1 / 12),
-                            ),
-                          );
-
-                          return PlatformWidget(
-                            material: (_, __) => InkWell(
-                              onTap: onTap,
-                              child: child,
-                            ),
-                            cupertino: (_, __) => GestureDetector(
-                              onTap: onTap,
-                              child: child,
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  ],
-                ),
+                  return PlatformWidget(
+                    material: (_, __) =>
+                        InkWell(
+                          onTap: onTap,
+                          child: child,
+                        ),
+                    cupertino: (_, __) =>
+                        GestureDetector(
+                          onTap: onTap,
+                          child: child,
+                        ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        );
+      })(),
     );
   }
 }
---
+++ lib/utils/bunny.dart
@@ -0,0 +1,5 @@
+// Keeps track on how often locations had no points in it
+// Should reset, once a location has points in it
+int emptyLocationsCount = 0;
+
+const EMPTY_LOCATION_THRESHOLD = 10;
