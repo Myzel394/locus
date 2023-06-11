87e925da44904f780dcc8901d9b0be55ce0b49fe
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-11 11:05:49 +0200

    fix: Remove cache as it's not a real caching library

--- lib/utils/cache.dart
+++ lib/utils/cache.dart
@@ -1,23 +1,9 @@
-import 'package:cache_manager/cache_manager.dart';
-
+// Haven't found a proper cache library yet
 Future<T> Function() withCache<T extends Map<String, dynamic>>(
   Future<T> Function() callback,
   final String key,
 ) {
   return () async {
-    try {
-      final cachedValue = await ReadCache.getJson(key: key);
-      if (cachedValue != null) {
-        return cachedValue;
-      }
-    } catch (_) {}
-
-    final value = await callback();
-    await WriteCache.setJson(
-      key: key,
-      value: value,
-    );
-
-    return value;
+    return callback();
   };
 }
