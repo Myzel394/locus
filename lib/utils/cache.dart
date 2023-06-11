d3a2bcd31af1dd3964f3a7bc2855bb7d78e2fbeb
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-11 11:15:19 +0200

    feat: Add memory based cache

--- lib/utils/cache.dart
+++ lib/utils/cache.dart
@@ -1,9 +1,19 @@
 // Haven't found a proper cache library yet
+final Map<String, Map<String, dynamic>> _CACHE = {};
+
 Future<T> Function() withCache<T extends Map<String, dynamic>>(
   Future<T> Function() callback,
   final String key,
 ) {
   return () async {
-    return callback();
+    if (_CACHE.containsKey(key)) {
+      return _CACHE[key] as T;
+    }
+
+    final value = await callback();
+
+    _CACHE[key] = value;
+
+    return value;
   };
 }
