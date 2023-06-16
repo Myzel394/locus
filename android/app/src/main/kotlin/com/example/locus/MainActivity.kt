6459b14b6977021995deb501a2a466a1154bfc1d
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-16 21:21:21 +0200

    debug: Use old FlutterActivity

--- android/app/src/main/kotlin/com/example/locus/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/locus/MainActivity.kt
@@ -1,6 +1,6 @@
 package app.myzel394.locus
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterFragmentActivity() {
+class MainActivity : FlutterActivity() {
 }
