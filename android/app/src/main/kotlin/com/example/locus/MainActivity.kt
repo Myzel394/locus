160b7f82bf2bf610ad73fdfeae6416a6de77c0be
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-16 17:43:27 +0200

    debug: undo changes

--- android/app/src/main/kotlin/com/example/locus/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/locus/MainActivity.kt
@@ -1,6 +1,6 @@
 package app.myzel394.locus
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
 }
