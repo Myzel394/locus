e4f73e596a97ea5d999434da077a4e23c902d7b7
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-23 23:24:02 +0200

    feat: Add contact picker

--- lib/screens/EmergencySetupScreen.dart
+++ lib/screens/EmergencySetupScreen.dart
@@ -4,6 +4,7 @@ import 'package:fluttercontactpicker/fluttercontactpicker.dart';
 import 'package:locus/constants/spacing.dart';
 import 'package:locus/screens/emergency_setup_screen_widgets/AddContactSheet.dart';
 import 'package:locus/services/SettingsService/settings_service.dart';
+import 'package:locus/utils/show_message.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:locus/services/SettingsService/contacts.dart' as contacts;
@@ -54,62 +55,81 @@ class _EmergencySetupScreenState extends State<EmergencySetupScreen> {
           child: Center(
             child: showHint
                 ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      const Icon(
-                        Icons.warning_sharp,
-                        color: Colors.yellow,
-                        size: 120,
-                      ),
-                      const SizedBox(height: LARGE_SPACE),
-                      Text(l10n.emergencySetup_hint),
-                      const SizedBox(height: LARGE_SPACE),
-                      PlatformTextButton(
-                        onPressed: () {
-                          setState(() {
-                            showHint = false;
-                          });
-                        },
-                        child: Text(l10n.emergencySetup_hint_understoodLabel),
-                      )
-                    ],
-                  )
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Icon(
+                  Icons.warning_sharp,
+                  color: Colors.yellow,
+                  size: 120,
+                ),
+                const SizedBox(height: LARGE_SPACE),
+                Text(l10n.emergencySetup_hint),
+                const SizedBox(height: LARGE_SPACE),
+                PlatformTextButton(
+                  onPressed: () {
+                    setState(() {
+                      showHint = false;
+                    });
+                  },
+                  child: Text(l10n.emergencySetup_hint_understoodLabel),
+                )
+              ],
+            )
                 : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      PlatformElevatedButton(
-                        onPressed: () async {
-                          final contacts.Contact? contact =
-                              await showPlatformModalSheet(
-                            context: context,
-                            material: MaterialModalSheetData(
-                              backgroundColor: Colors.transparent,
-                              isScrollControlled: true,
-                              isDismissible: true,
-                            ),
-                            builder: (_) => const AddContactScreen(),
-                          );
-                        },
-                        material: (_, __) => MaterialElevatedButtonData(
-                          icon: const Icon(Icons.add_circle_rounded),
-                        ),
-                        child: Text(l10n.emergencySetup_addContact_label),
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                PlatformElevatedButton(
+                  onPressed: () async {
+                    final contacts.Contact? contact =
+                    await showPlatformModalSheet(
+                      context: context,
+                      material: MaterialModalSheetData(
+                        backgroundColor: Colors.transparent,
+                        isScrollControlled: true,
+                        isDismissible: true,
                       ),
-                      const SizedBox(height: MEDIUM_SPACE),
-                      PlatformElevatedButton(
-                        onPressed: () async {
-                          final PhoneContact contact =
-                              await FlutterContactPicker.pickPhoneContact();
-                        },
-                        material: (_, __) => MaterialElevatedButtonData(
-                          icon: const Icon(Icons.person),
-                        ),
-                        child: Text(l10n.emergencySetup_pickContact_label),
-                      )
-                    ],
-                  ),
+                      builder: (_) => const AddContactScreen(),
+                    );
+                  },
+                  material: (_, __) =>
+                      MaterialElevatedButtonData(
+                        icon: const Icon(Icons.add_circle_rounded),
+                      ),
+                  child: Text(l10n.emergencySetup_addContact_label),
+                ),
+                const SizedBox(height: MEDIUM_SPACE),
+                PlatformElevatedButton(
+                  onPressed: () async {
+                    final PhoneContact rawContact =
+                    await FlutterContactPicker.pickPhoneContact();
+
+                    if (!mounted) {
+                      return;
+                    }
+
+                    if (rawContact.fullName == null ||
+                        rawContact.phoneNumber != null) {
+                      showMessage(
+                        context,
+                        l10n.emergencySetup_pickContact_error_contactInvalid,
+                      );
+                    }
+
+                    final contact = contacts.Contact(
+                      name: rawContact.fullName!,
+                      phoneNumber: rawContact.phoneNumber!.number!,
+                    );
+                  },
+                  material: (_, __) =>
+                      MaterialElevatedButtonData(
+                        icon: const Icon(Icons.person),
+                      ),
+                  child: Text(l10n.emergencySetup_pickContact_label),
+                )
+              ],
+            ),
           ),
         ),
       ),
