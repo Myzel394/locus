aef9b81e15b7c95c8664b5ae13a7a133e14ee875
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-07-07 22:04:38 +0200

    fix: Handle empty next end dates

--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -102,12 +102,11 @@ class Task extends ChangeNotifier with LocationBase {
     };
   }
 
-  static Future<Task> create(
-    final String name,
-    final List<String> relays, {
-    List<TaskRuntimeTimer> timers = const [],
-    bool deleteAfterRun = false,
-  }) async {
+  static Future<Task> create(final String name,
+      final List<String> relays, {
+        List<TaskRuntimeTimer> timers = const [],
+        bool deleteAfterRun = false,
+      }) async {
     FlutterLogs.logInfo(
       LOG_TAG,
       "Task",
@@ -120,7 +119,9 @@ class Task extends ChangeNotifier with LocationBase {
       id: uuid.v4(),
       name: name,
       encryptionPassword: secretKey,
-      nostrPrivateKey: Keychain.generate().private,
+      nostrPrivateKey: Keychain
+          .generate()
+          .private,
       relays: relays,
       createdAt: DateTime.now(),
       timers: timers,
@@ -175,7 +176,7 @@ class Task extends ChangeNotifier with LocationBase {
   Future<bool> shouldRunNow() async {
     final executionStatus = await getExecutionStatus();
     final shouldRunNowBasedOnTimers =
-        timers.any((timer) => timer.shouldRun(DateTime.now()));
+    timers.any((timer) => timer.shouldRun(DateTime.now()));
 
     if (shouldRunNowBasedOnTimers) {
       return true;
@@ -243,7 +244,7 @@ class Task extends ChangeNotifier with LocationBase {
   Future<DateTime?> startScheduleTomorrow() {
     final tomorrow = DateTime.now().add(const Duration(days: 1));
     final nextDate =
-        DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 6, 0, 0);
+    DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 6, 0, 0);
 
     return startSchedule(startDate: nextDate);
   }
@@ -348,8 +349,7 @@ class Task extends ChangeNotifier with LocationBase {
   // 2. Encrypt the task with the password
   // 3. Publish the encrypted task to a random Nostr relay
   // 4. Generate a link that contains the password and the Nostr relay ID
-  Future<String> generateLink(
-    final String host, {
+  Future<String> generateLink(final String host, {
     final void Function(TaskLinkPublishProgress progress)? onProgress,
   }) async {
     onProgress?.call(TaskLinkPublishProgress.startsSoon);
@@ -398,8 +398,7 @@ class Task extends ChangeNotifier with LocationBase {
   }
 
   Future<void> publishLocation(
-    final LocationPointService locationPoint,
-  ) async {
+      final LocationPointService locationPoint,) async {
     final eventManager = NostrEventsManager.fromTask(this);
 
     final rawMessage = jsonEncode(locationPoint.toJSON());
@@ -480,7 +479,7 @@ class TaskService extends ChangeNotifier {
     // await all `toJson` functions
     final data = await Future.wait<Map<String, dynamic>>(
       _tasks.map(
-        (task) => task.toJSON(),
+            (task) => task.toJSON(),
       ),
     );
 
@@ -613,19 +612,28 @@ DateTime? findNextStartDate(final List<TaskRuntimeTimer> timers,
   return nextDates.first;
 }
 
-DateTime? findNextEndDate(final List<TaskRuntimeTimer> timers,
-    {final DateTime? startDate}) {
+DateTime? findNextEndDate(final List<TaskRuntimeTimer> timers, {
+  final DateTime? startDate,
+}) {
   final now = startDate ?? DateTime.now();
   final nextDates = List<DateTime>.from(
     timers.map((timer) => timer.nextEndDate(now)).where((date) => date != null),
-  )..sort();
+  )
+    ..sort();
+
+  if (nextDates.isEmpty) {
+    return null;
+  }
 
   DateTime endDate = nextDates.first;
 
   for (final date in nextDates.sublist(1)) {
     final nextStartDate = findNextStartDate(timers, startDate: date);
     if (nextStartDate == null ||
-        nextStartDate.difference(date).inMinutes.abs() > 15) {
+        nextStartDate
+            .difference(date)
+            .inMinutes
+            .abs() > 15) {
       // No next start date found or the difference is more than 15 minutes, so this is the last date
       break;
     }
