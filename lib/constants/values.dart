6abc41248112de65793293af7027a9ef8b84db47
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-07-14 16:55:32 +0200

    fix: Increase real time update threshold

--- lib/constants/values.dart
+++ lib/constants/values.dart
@@ -8,7 +8,7 @@ const APK_RELEASES_URL = "https://github.com/Myzel394/locus/releases";
 const BACKGROUND_LOCATION_UPDATES_MINIMUM_DISTANCE_FILTER = 25;
 
 const LOCATION_FETCH_TIME_LIMIT = Duration(minutes: 5);
-const LOCATION_INTERVAL = Duration(minutes: 2);
+const LOCATION_INTERVAL = Duration(minutes: 1);
 
 const TRANSFER_DATA_USERNAME = "locus_transfer";
 final TRANSFER_SUCCESS_MESSAGE = Uint8List.fromList([1, 2, 3, 4]);
