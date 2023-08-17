dd1a40b753801801f74900e999db97ecc941d8ae
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-08-17 23:18:06 +0200

    fix: Fix protocol handling

--- lib/api/nostr-relays.dart
+++ lib/api/nostr-relays.dart
@@ -5,11 +5,13 @@ import "package:http/http.dart" as http;
 import 'package:locus/constants/apis.dart';
 import 'package:locus/constants/values.dart';
 
+import '../widgets/RelaySelectSheet.dart';
+
 final RELAY_FETCHER_FUNCTIONS = [
   NostrWatchAPI.getPublicNostrRelays,
   NostrBandAPI.getTrendingProfiles,
   NostrWatchAPI.getAllNostrRelays,
-      () => Future.value(FALLBACK_RELAYS),
+  () => Future.value(FALLBACK_RELAYS),
 ];
 
 // Tries each of the fallback relays until one works
@@ -73,40 +75,15 @@ class NostrBandAPI {
         List<dynamic>.from(jsonDecode(response.body)["profiles"])
             .map((e) => e["relays"])
             .expand((element) => element)
-            .toSet());
+            .toSet()
+            .map((element) {
+      final rawDomain = DOMAIN_REPLACE_REGEX.firstMatch(element);
+
+      if (rawDomain == null) {
+        return null;
+      }
+
+      return addProtocol(rawDomain.group(1)!);
+    }).where((element) => element != null));
   }
 }
-
-// Top 30 most used free relays
-const FALLBACK_RELAYS = [
-  "relay.damus.io",
-  "eden.nostr.land",
-  "nos.lol",
-  "relay.snort.social",
-  "relay.current.fyi",
-  "brb.io",
-  "nostr.orangepill.dev",
-  "nostr-pub.wellorder.net",
-  "nostr.bitcoiner.social",
-  "nostr.wine",
-  "nostr.oxtr.dev",
-  "relay.nostr.bg",
-  "nostr.mom",
-  "nostr.fmt.wiz.biz",
-  "relay.nostr.band",
-  "nostr-pub.semisol.dev",
-  "nostr.milou.lol",
-  "nostr.onsats.org",
-  "relay.nostr.info",
-  "puravida.nostr.land",
-  "offchain.pub",
-  "relay.orangepill.dev",
-  "no.str.cr",
-  "nostr.zebedee.cloud",
-  "atlas.nostr.land",
-  "nostr-relay.wlvs.space",
-  "relay.nostrati.com",
-  "relay.nostr.com.au",
-  "relay.inosta.cc",
-  "nostr.rocks",
-];
--- lib/constants/apis.dart
+++ lib/constants/apis.dart
@@ -1,4 +1,43 @@
+import 'package:locus/widgets/RelaySelectSheet.dart';
+
 const NOSTR_PUBLIC_RELAYS_LIST_URI = "https://api.nostr.watch/v1/public";
 const NOSTR_ONLINE_RELAYS_LIST_URI = "https://api.nostr.watch/v1/online";
 const NOSTR_TRENDING_PROFILES_URI =
     "https://api.nostr.band/v0/trending/profiles";
+
+// Top 30 most used free relays
+final FALLBACK_RELAYS = [
+  "relay.damus.io",
+  "eden.nostr.land",
+  "nos.lol",
+  "relay.snort.social",
+  "relay.current.fyi",
+  "brb.io",
+  "nostr.orangepill.dev",
+  "nostr-pub.wellorder.net",
+  "nostr.bitcoiner.social",
+  "nostr.wine",
+  "nostr.oxtr.dev",
+  "relay.nostr.bg",
+  "nostr.mom",
+  "nostr.fmt.wiz.biz",
+  "relay.nostr.band",
+  "nostr-pub.semisol.dev",
+  "nostr.milou.lol",
+  "nostr.onsats.org",
+  "relay.nostr.info",
+  "puravida.nostr.land",
+  "offchain.pub",
+  "relay.orangepill.dev",
+  "no.str.cr",
+  "nostr.zebedee.cloud",
+  "atlas.nostr.land",
+  "nostr-relay.wlvs.space",
+  "relay.nostrati.com",
+  "relay.nostr.com.au",
+  "relay.inosta.cc",
+  "nostr.rocks",
+].map(addProtocol).toList();
+
+// Strip everything after the domain for regex
+final DOMAIN_REPLACE_REGEX = RegExp(r"(wss:\/\/[-\w.]+)(?:\/.*)?");
