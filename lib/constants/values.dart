e4a0d1366ea12289587a92527243f1ca3cc34de4
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-10-11 20:24:19 +0200

    refactor: Move InitLocationFromSettings.dart into own widget

--- lib/constants/values.dart
+++ lib/constants/values.dart
@@ -5,7 +5,7 @@ const TRANSLATION_HELP_URL = "https://github.com/Myzel394/locus";
 const DONATION_URL = "https://github.com/Myzel394/locus";
 const APK_RELEASES_URL = "https://github.com/Myzel394/locus/releases";
 
-const BACKGROUND_LOCATION_UPDATES_MINIMUM_DISTANCE_FILTER = 50;
+const BACKGROUND_LOCATION_UPDATES_MINIMUM_DISTANCE_FILTER = 30;
 
 const LOCATION_FETCH_TIME_LIMIT = Duration(minutes: 5);
 const LOCATION_INTERVAL = Duration(minutes: 1);
