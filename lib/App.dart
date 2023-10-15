0c41598c224d30bd921ac5c2a303cd83632e26f7
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-10-15 17:05:35 +0200

    fix: Remove unnecessary Expended

--- lib/App.dart
+++ lib/App.dart
@@ -62,100 +62,42 @@ class App extends StatelessWidget {
       child: DynamicColorBuilder(
         builder:
             (ColorScheme? lightColorScheme, ColorScheme? darkColorScheme) =>
-                Expanded(
-          child: PlatformApp(
-            title: 'Locus',
-            material: (_, __) => MaterialAppData(
-              theme: (() {
-                if (lightColorScheme != null) {
-                  return LIGHT_THEME_MATERIAL.copyWith(
-                    colorScheme: settings.primaryColor == null
-                        ? lightColorScheme
-                        : createColorScheme(
-                            lightColorScheme,
-                            settings.primaryColor!,
-                            Brightness.light,
-                          ),
-                    primaryColor:
-                        settings.primaryColor ?? lightColorScheme.primary,
-                  );
-                }
-
+                PlatformApp(
+          title: 'Locus',
+          material: (_, __) => MaterialAppData(
+            theme: (() {
+              if (lightColorScheme != null) {
                 return LIGHT_THEME_MATERIAL.copyWith(
                   colorScheme: settings.primaryColor == null
-                      ? null
+                      ? lightColorScheme
                       : createColorScheme(
-                          lightColorScheme ??
-                              ColorScheme.fromSwatch(
-                                primarySwatch:
-                                    createMaterialColor(settings.primaryColor!),
-                              ),
+                          lightColorScheme,
                           settings.primaryColor!,
                           Brightness.light,
                         ),
-                  primaryColor: settings.primaryColor,
+                  primaryColor:
+                      settings.primaryColor ?? lightColorScheme.primary,
                 );
-              })(),
-              darkTheme: (() {
-                if (settings.getAndroidTheme() == AndroidTheme.miui) {
-                  return DARK_THEME_MATERIAL_MIUI.copyWith(
-                    colorScheme: settings.primaryColor == null
-                        ? null
-                        : createColorScheme(
-                            const ColorScheme.dark(),
-                            settings.primaryColor!,
-                            Brightness.dark,
-                          ),
-                    primaryColor: settings.primaryColor,
-                    elevatedButtonTheme: ElevatedButtonThemeData(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            settings.primaryColor ?? MIUI_PRIMARY_COLOR,
-                        foregroundColor: Colors.white,
-                        splashFactory: NoSplash.splashFactory,
-                        textStyle: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700,
-                        ),
+              }
+
+              return LIGHT_THEME_MATERIAL.copyWith(
+                colorScheme: settings.primaryColor == null
+                    ? null
+                    : createColorScheme(
+                        lightColorScheme ??
+                            ColorScheme.fromSwatch(
+                              primarySwatch:
+                                  createMaterialColor(settings.primaryColor!),
+                            ),
+                        settings.primaryColor!,
+                        Brightness.light,
                       ),
-                    ),
-                  );
-                }
-
-                if (darkColorScheme != null) {
-                  return DARK_THEME_MATERIAL.copyWith(
-                    colorScheme: settings.primaryColor == null
-                        ? darkColorScheme
-                        : createColorScheme(
-                            darkColorScheme,
-                            settings.primaryColor!,
-                            Brightness.dark,
-                          ),
-                    primaryColor:
-                        settings.primaryColor ?? darkColorScheme.primary,
-                    scaffoldBackgroundColor: HSLColor.fromColor(
-                            settings.primaryColor ?? darkColorScheme.background)
-                        .withLightness(0.08)
-                        .toColor(),
-                    dialogBackgroundColor: settings.primaryColor == null
-                        ? darkColorScheme.background
-                        : HSLColor.fromColor(settings.primaryColor!)
-                            .withLightness(0.15)
-                            .toColor(),
-                    inputDecorationTheme:
-                        DARK_THEME_MATERIAL.inputDecorationTheme.copyWith(
-                      fillColor: settings.primaryColor == null
-                          ? null
-                          : HSLColor.fromColor(settings.primaryColor!)
-                              .withLightness(0.3)
-                              .withSaturation(.5)
-                              .toColor(),
-                    ),
-                  );
-                }
-
-                return DARK_THEME_MATERIAL.copyWith(
+                primaryColor: settings.primaryColor,
+              );
+            })(),
+            darkTheme: (() {
+              if (settings.getAndroidTheme() == AndroidTheme.miui) {
+                return DARK_THEME_MATERIAL_MIUI.copyWith(
                   colorScheme: settings.primaryColor == null
                       ? null
                       : createColorScheme(
@@ -164,13 +106,39 @@ class App extends StatelessWidget {
                           Brightness.dark,
                         ),
                   primaryColor: settings.primaryColor,
-                  scaffoldBackgroundColor: settings.primaryColor == null
-                      ? null
-                      : HSLColor.fromColor(settings.primaryColor!)
-                          .withLightness(0.08)
-                          .toColor(),
+                  elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          settings.primaryColor ?? MIUI_PRIMARY_COLOR,
+                      foregroundColor: Colors.white,
+                      splashFactory: NoSplash.splashFactory,
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                );
+              }
+
+              if (darkColorScheme != null) {
+                return DARK_THEME_MATERIAL.copyWith(
+                  colorScheme: settings.primaryColor == null
+                      ? darkColorScheme
+                      : createColorScheme(
+                          darkColorScheme,
+                          settings.primaryColor!,
+                          Brightness.dark,
+                        ),
+                  primaryColor:
+                      settings.primaryColor ?? darkColorScheme.primary,
+                  scaffoldBackgroundColor: HSLColor.fromColor(
+                          settings.primaryColor ?? darkColorScheme.background)
+                      .withLightness(0.08)
+                      .toColor(),
                   dialogBackgroundColor: settings.primaryColor == null
-                      ? null
+                      ? darkColorScheme.background
                       : HSLColor.fromColor(settings.primaryColor!)
                           .withLightness(0.15)
                           .toColor(),
@@ -184,60 +152,90 @@ class App extends StatelessWidget {
                             .toColor(),
                   ),
                 );
-              })(),
-              themeMode: ThemeMode.system,
-            ),
-            cupertino: (_, __) => CupertinoAppData(
-              theme: settings.primaryColor == null
-                  ? LIGHT_THEME_CUPERTINO
-                  : LIGHT_THEME_CUPERTINO.copyWith(
-                      primaryColor: settings.primaryColor,
-                    ),
-            ),
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-            builder: (context, child) => Stack(
-              children: [
-                const UpdateLocationHistory(),
-                const UniLinksHandler(),
-                const UpdateLastLocationToSettings(),
-                const RegisterBackgroundListeners(),
-                const UpdateLocaleToSettings(),
-                const HandleNotifications(),
-                const CheckViewAlarmsLive(),
-                const ManageQuickActions(),
-                const InitCurrentLocationFromSettings(),
-                const ShowUpdateDialog(),
-                const PublishTaskPositionsOnUpdate(),
-                if (child != null) child,
-              ],
-            ),
-            onGenerateRoute: (routeSettings) {
-              final screen = (() {
-                if (settings.getRequireBiometricAuthenticationOnStart()) {
-                  return const BiometricsRequiredStartupScreen();
-                }
-
-                if (!settings.userHasSeenWelcomeScreen) {
-                  return const WelcomeScreen();
-                }
-
-                return const LocationsOverviewScreen();
-              })();
-
-              if (isCupertino(context)) {
-                return MaterialWithModalsPageRoute(
-                  builder: (_) => screen,
-                  settings: routeSettings,
-                );
               }
 
-              return NativePageRoute(
-                builder: (_) => screen,
-                context: context,
+              return DARK_THEME_MATERIAL.copyWith(
+                colorScheme: settings.primaryColor == null
+                    ? null
+                    : createColorScheme(
+                        const ColorScheme.dark(),
+                        settings.primaryColor!,
+                        Brightness.dark,
+                      ),
+                primaryColor: settings.primaryColor,
+                scaffoldBackgroundColor: settings.primaryColor == null
+                    ? null
+                    : HSLColor.fromColor(settings.primaryColor!)
+                        .withLightness(0.08)
+                        .toColor(),
+                dialogBackgroundColor: settings.primaryColor == null
+                    ? null
+                    : HSLColor.fromColor(settings.primaryColor!)
+                        .withLightness(0.15)
+                        .toColor(),
+                inputDecorationTheme:
+                    DARK_THEME_MATERIAL.inputDecorationTheme.copyWith(
+                  fillColor: settings.primaryColor == null
+                      ? null
+                      : HSLColor.fromColor(settings.primaryColor!)
+                          .withLightness(0.3)
+                          .withSaturation(.5)
+                          .toColor(),
+                ),
               );
-            },
+            })(),
+            themeMode: ThemeMode.system,
           ),
+          cupertino: (_, __) => CupertinoAppData(
+            theme: settings.primaryColor == null
+                ? LIGHT_THEME_CUPERTINO
+                : LIGHT_THEME_CUPERTINO.copyWith(
+                    primaryColor: settings.primaryColor,
+                  ),
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          builder: (context, child) => Stack(
+            children: [
+              const UpdateLocationHistory(),
+              const UniLinksHandler(),
+              const UpdateLastLocationToSettings(),
+              const RegisterBackgroundListeners(),
+              const UpdateLocaleToSettings(),
+              const HandleNotifications(),
+              const CheckViewAlarmsLive(),
+              const ManageQuickActions(),
+              const InitCurrentLocationFromSettings(),
+              const ShowUpdateDialog(),
+              const PublishTaskPositionsOnUpdate(),
+              if (child != null) child,
+            ],
+          ),
+          onGenerateRoute: (routeSettings) {
+            final screen = (() {
+              if (settings.getRequireBiometricAuthenticationOnStart()) {
+                return const BiometricsRequiredStartupScreen();
+              }
+
+              if (!settings.userHasSeenWelcomeScreen) {
+                return const WelcomeScreen();
+              }
+
+              return const LocationsOverviewScreen();
+            })();
+
+            if (isCupertino(context)) {
+              return MaterialWithModalsPageRoute(
+                builder: (_) => screen,
+                settings: routeSettings,
+              );
+            }
+
+            return NativePageRoute(
+              builder: (_) => screen,
+              context: context,
+            );
+          },
         ),
       ),
     );
